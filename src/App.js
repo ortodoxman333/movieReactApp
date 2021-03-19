2d133d1e36b51d58e2817da2aae8b3c597f07844
SALEEE <sasadj333@yahoo.com>
2021-03-19 20:12:55 +0100

    added  redirect file and formated code

--- src/App.js
+++ src/App.js
@@ -1,64 +1,69 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import Movie from "./components/Movie";
 
-const FEATURED_API = " https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
-const SEARCH_API = "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query="
-
-
+const FEATURED_API =
+  " https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+const SEARCH_API =
+  "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
 
 function App() {
   const [movies, setMovies] = useState([]);
-  const [searchTerm, setSearchTerm] = useState('');
-  
+  const [searchTerm, setSearchTerm] = useState("");
+
   useEffect(() => {
     getMovies(FEATURED_API);
-  },[]);
+  }, []);
 
   const getMovies = (API) => {
     fetch(API)
-    .then((res) => res.json())
-    .then(data => {
-      setMovies(data.results);
-    });
-  }
+      .then((res) => res.json())
+      .then((data) => {
+        setMovies(data.results);
+      });
+  };
   const handleOnSubmit = (e) => {
     e.preventDefault();
 
     if (searchTerm) {
       getMovies(SEARCH_API + searchTerm);
-    fetch(SEARCH_API + searchTerm)
-         .then((res) => res.json ())
-         .then((data) => {
-           setMovies(data.results);
-         });
+      fetch(SEARCH_API + searchTerm)
+        .then((res) => res.json())
+        .then((data) => {
+          setMovies(data.results);
+        });
 
-              setSearchTerm('');
-        }
+      setSearchTerm("");
+    }
   };
 
   const handleOnChange = (e) => {
     setSearchTerm(e.target.value);
-  }
+  };
 
   return (
     <>
-    <header>
-      <form onSubmit={handleOnSubmit}>
-    <input 
-    className="search"
-    type="search" 
-    placeholder="Search..."
-    value={searchTerm}
-    onChange={handleOnChange}
-     />
-</form>
-</header>
-  <div className="movie-container">
-    {movies.length > 0 &&
-     movies.map((movie)  => <Movie key={movie.id} 
-     {...movie} /> )}
-     </div>
-     </>
+      <header>
+        <div className="person">
+          <img src={`${process.env.PUBLIC_URL}/slovo-s.png`} alt="person" />
+        </div>
+        <div className="titlehead">
+          <h1>earchMovie</h1>
+        </div>
+        <form onSubmit={handleOnSubmit}>
+          <input
+            className="search"
+            type="search"
+            placeholder="Search..."
+            value={searchTerm}
+            onChange={handleOnChange}
+          />
+        </form>
+      </header>
+      <div className="movie-container">
+        {movies.length > 0 &&
+          movies.map((movie) => <Movie key={movie.id} {...movie} />)}
+      </div>
+    </>
   );
 }
 
